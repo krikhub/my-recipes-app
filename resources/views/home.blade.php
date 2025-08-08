1c42e08b1524ae41b6eab24e60074b3edee287ea
krikhub <info@alexanderkrikun.de>
2025-08-08 16:24:07 +0200

    add no results

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,13 @@
         <p class="text-sm text-gray-400 mb-4">Anzahl: {{ $recipes->count() }}</p>
 
        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            @if(isset($query))
-                <h2>Search Results:</h2>
+            @if($recipes->isEmpty())
+                <p>Keine Ergebnisse gefunden.</p>
+            @else
+                @foreach($recipes as $recipe)
+                    <x-utils.recipe-card :recipe="$recipe" />
+                @endforeach
             @endif
-            @foreach($recipes as $recipe)
-                <x-utils.recipe-card :recipe="$recipe" />
-            @endforeach
         </div>
     </x-layouts.max-width>
 </x-layouts.base>
